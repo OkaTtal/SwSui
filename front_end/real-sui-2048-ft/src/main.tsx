2c2db9da5eb9137473978d49a54600753c25a9e4
oka <434291091spd@gmail.com>
2025-05-10 17:08:17 +0800

    SwSui real demo 1

---
+++ front_end/real-sui-2048-ft/src/main.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './game'
+import './index.css'
+import '@mysten/dapp-kit/dist/index.css';
+
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+const networks = {
+	localnet: { url: getFullnodeUrl('localnet') },
+	devnet: { url: getFullnodeUrl('devnet') },
+	testnet: { url: getFullnodeUrl('testnet') },
+	mainnet: { url: getFullnodeUrl('mainnet') },
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<SuiClientProvider networks={networks} defaultNetwork="testnet">
+				<WalletProvider>
+					<App />
+				</WalletProvider>
+			</SuiClientProvider>
+		</QueryClientProvider>
+	</React.StrictMode>,
+);
